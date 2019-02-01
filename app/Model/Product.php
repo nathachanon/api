cf4168697716a6356741bbd3d8a685fbcd8b4028
Nathachanon <nathachanon.s@gmail.com>
2019-02-01 12:15:58 +0700

    Product update

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+	protected $fillable = [
+		'name','detail','stock','price','discount'
+
+	];
+
     public function reviews()
     {
       return $this->hasMany(Reviews::class);
